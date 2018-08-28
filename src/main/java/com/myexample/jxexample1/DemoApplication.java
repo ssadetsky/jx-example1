9b8d13abfb7d4d49a98593bec9d9d741fe341ed9
ssadetsky <sadetsky@gmail.com>
2018-08-28 13:32:47 -0700

    Initial import

---
+++ src/main/java/com/myexample/jxexample1/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.myexample.jxexample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
